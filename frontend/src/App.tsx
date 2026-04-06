3cf5b026b641a653ca9e6bd66c3d34aef02e888d
Greg <gregminezzi@gmail.com>
2026-04-06 14:58:13 +0200

    initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-black">
+      <div className="h-screen flex items-center justify-center text-8xl text-blue-500">
+        Chingu 60: 3-33
+      </div>
+    </div>
+  );
+}
