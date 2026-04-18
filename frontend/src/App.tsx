7e6a742f2713291e6b0df26325b1d9c734f3dddd
Tibamwenda Anthony <anthonyngisiro@gmail.com>
2026-04-18 14:40:21 +0300

    feat(auth): Implement Account Creation UI with Zod and React Hook Form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
+import AccountCreationPage from './pages/Auth/AccountCreationPage';
+
 export default function App() {
   return (
-    <div className="bg-black">
-      <div className="h-screen flex items-center justify-center text-8xl text-blue-500">
-        Chingu 60: 3-33
-      </div>
-    </div>
+    <AccountCreationPage />
   );
 }
+
